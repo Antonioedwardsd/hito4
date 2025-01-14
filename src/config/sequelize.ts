25c519feb09ed076cbe02ca613a03f017c8597fe
Antonio Edwards <156668143+Antonioedwardsd@users.noreply.github.com>
2025-01-14 17:37:11 -0300

    sequelize.ts

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,18 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
-import path from "path";
-import { fileURLToPath } from "url";
 import User from "../models/user.model";
 import Task from "../models/task.model";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 export const sequelize = new Sequelize({
 	dialect: "postgres",
 	host: "localhost",
 	username: "postgres",
 	password: "root",
-	database: "usersdb",
+	database: "hito_4",
 	port: 5434,
 	models: [User, Task],
 	logging: false,
